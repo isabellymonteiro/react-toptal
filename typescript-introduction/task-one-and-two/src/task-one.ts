2f7e35f732060a35942eca89afea13f23c37064e
Isabelly Monteiro <isabelly.monteiro@toptal.com>
2022-12-27 10:20:36 -0300

    Add date type suggestion

--- typescript-introduction/task-one-and-two/src/task-one.ts
+++ typescript-introduction/task-one-and-two/src/task-one.ts
@@ -53,3 +53,31 @@ const handleResponse = (response: ApiResponse) => {
   console.log(response.data)
 }
 */
+
+// Date type suggestion (I'm not using but good for the future)
+
+// You can use template literals to define types
+
+// mm/dd/yyy
+type CustomDate = `${number}/${number}/${number}`
+
+const myDate: CustomDate = '12/12/2000' // valid
+// const myDate2: CustomDate = '12-12-2000' // invalid
+const myDate3: CustomDate = '123123/123123123/123123123' // valid
+
+
+// more specific types
+type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+type Day = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31
+type CustomSpecificDate = `${Month}/${Day}/${number}`
+
+const myOtherDate: CustomSpecificDate = '12/12/2000' // valid
+// const myOtherDate2: CustomSpecificDate = '12-12-2000' // invalid
+// const myOtherDate3: CustomSpecificDate = '123123/123123123/123123123' // invalid
+
+// we still have problems in cases like
+const myOtherDate4: CustomSpecificDate = '11/31/2000' // there's no 31th in november, but is still valid
+const myOtherDate5: CustomSpecificDate = '11/30/-2000' // -2000 it's a valid number
+
+// there's no perfect approach.
+// one thing you can do is to use one of these types and check the format in runtime
