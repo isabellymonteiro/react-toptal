faac7546f8767fe4a5544dc8e8efe97879a20b73
Isabelly Monteiro <isabelly.monteiro@toptal.com>
2022-12-28 13:41:44 -0300

    Remove repeated button children prop

--- react-styling/alaska/src/components/Button/Button.tsx
+++ react-styling/alaska/src/components/Button/Button.tsx
@@ -1,19 +1,17 @@
-import { ButtonHTMLAttributes, ReactNode } from 'react'
+import { ButtonHTMLAttributes } from 'react'
 import * as S from './styles'
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: ReactNode
   variant: 'outlined' | 'contained'
   color: 'primary' | 'secondary' | 'warning' | 'danger'
   fullWidth?: boolean
 }
 
-const Button = ({ children, variant, color, fullWidth = false, ...props }: ButtonProps) => {
-  return (
-    <S.Button {...props} variant={variant} color={color} fullWidth={fullWidth}>
-      {children}
-    </S.Button>
-  )
-}
+const Button = ({ 
+  variant,
+  color,
+  fullWidth = false,
+  ...props
+}: ButtonProps) => <S.Button {...props} variant={variant} color={color} fullWidth={fullWidth} />
 
 export default Button
