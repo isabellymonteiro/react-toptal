008d3ca54f3cebf102c5e8ef0495e465225f8d90
Isabelly Monteiro <82273361+isabellymonteiro@users.noreply.github.com>
2023-01-19 23:03:19 -0300

    Refactor TileCounter test

--- react-introduction/task-one/src/components/TileCounter/TileCounter.test.jsx
+++ react-introduction/task-one/src/components/TileCounter/TileCounter.test.jsx
@@ -10,38 +10,12 @@ const tileCounterMock = {
 const renderComponent = () => render(<TileCounter {...tileCounterMock} />)
 
 describe('TileCounter', () => {
-  describe('when the page loads', () => {
-    it('renders with props', () => {
-      renderComponent()
-  
-      expect(screen.getByTestId(TestId.Root)).toBeInTheDocument()
-    })
-  
-    it('renders the correct count', () => {
-      renderComponent()
-  
-      const count = screen.getByTestId(TestId.Count)
-  
-      expect(count).toBeInTheDocument()
-      expect(count).toHaveTextContent(tileCounterMock.tileCount)
-    })
-  })
+  it('renders with the correct props', () => {
+    renderComponent()
 
-  /* how to do it? 
-  describe('when the counter buttons are clicked', () => {
-    it('increments the counter by 1 if the increment button is clicked', () => {
-      renderComponent()
-      
-      const incrementButton = screen.getByText('-')
-      fireEvent.click(incrementButton)
+    const count = screen.getByTestId(TestId.Count)
 
-      const count = screen.getByTestId(TestId.Count)
-
-      expect(count).toHaveTextContent(`${tileCounterMock.tileCount + 1}`)
-    })
-
-    it('decrements the counter by 1 if the decrement button is clicked', () => {
-      
-    })
-  }) */
+    expect(count).toBeInTheDocument()
+    expect(count).toHaveTextContent(tileCounterMock.tileCount)
+  }) 
 })
