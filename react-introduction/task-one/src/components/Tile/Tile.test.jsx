374a40b73f26ea0571daa29daa780977c95fb3b1
Isabelly Monteiro <isabelly.monteiro@toptal.com>
2023-01-19 21:06:50 -0300

    Add tile unit test

--- react-introduction/task-one/src/components/Tile/Tile.test.jsx
+++ react-introduction/task-one/src/components/Tile/Tile.test.jsx
@@ -1,16 +1,45 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Tile, { TestId } from '../Tile/Tile'
 
 describe('Tile', () => {
-  it('renders without color prop', () => {
-    render(<Tile />)
+  describe('when it renders', () => {
+    it('without color prop', () => {
+      render(<Tile />)
 
-    expect(screen.getByTestId(TestId.Root)).toBeInTheDocument()
+      expect(screen.getByTestId(TestId.Root)).toBeInTheDocument()
+    })
+  
+    it('with color prop', () => {
+      render(<Tile color='#5FBFCA' />)
+
+      expect(screen.getByTestId(TestId.Root)).toBeInTheDocument()
+    })
   })
 
-  it('renders with color prop', () => {
-    render(<Tile color='#5FBFCA' />)
+  describe('when it is clicked', () => {
+    it('shows the color if the color was hidden', () => {
+      render(<Tile color='#5FBFCA' />)
 
-    expect(screen.getByTestId(TestId.Root)).toBeInTheDocument()
+      const tile = screen.getByTestId(TestId.Root)
+
+      expect(tile).not.toHaveTextContent('#5FBFCA')
+
+      fireEvent.click(tile)
+      expect(tile).toHaveTextContent('#5FBFCA')
+    })
+  
+    it('hides the color if it was shown', () => {
+      render(<Tile color='#5FBFCA' />)
+
+      const tile = screen.getByTestId(TestId.Root)
+
+      expect(tile).not.toHaveTextContent('#5FBFCA')
+
+      fireEvent.click(tile)
+      expect(tile).toHaveTextContent('#5FBFCA')
+
+      fireEvent.click(tile)
+      expect(tile).not.toHaveTextContent('#5FBFCA')
+    })
   })
 })
